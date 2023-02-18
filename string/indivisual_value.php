13ce7dd775edb66ce2542eab7a84f61633053527
souvik mondal <38136428+KALYANIHF@users.noreply.github.com>
2023-02-18 22:52:28 +0530

    changes

--- string/indivisual_value.php
+++ string/indivisual_value.php
@@ -1,6 +1,22 @@
 <?php
-$str = "this is the most beautiful thing i have seen";
-$length = strlen($str);
-for ($i = 0; $i < $length; $i++) {
-    printf("The %dth chractor is %s <br/>", $i, $str[$i]);
+if (isset($_POST['submit'])) {
+    $name = htmlspecialchars($_POST['name']);
+    echo $name;
 }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP special chracter</title>
+</head>
+<body>
+    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+    <label for="name">Name:</label>
+    <input type="text" name="name" id="name">
+    <input type="submit" name="submit" value="submit">
+    </form>
+</body>
+</html>
--- string/interpolation.php
+++ string/interpolation.php
@@ -7,9 +7,9 @@
  * elcape caractors in php
  * % to start the chractor to perse in php
  * %b -- it's a integer value that perse a boolean value in string
- * %c --  it's a int value that perse a chractor in the string
+ * %c -- it's a int value that perse a chractor in the string
  * %d -- it's a int value that perse a decimal value in the string
- * %e --  it's a double value that perse a scintific charator (e^(value)) like this format
+ * %e -- it's a double value that perse a scintific charator (e^(value)) like this format
  * %E -- it's a double value that perse a scintific chractor (E^(value)) like format
  * %f -- it's a float value that perse in small chractor format
  * %F -- it's a float value that perse in capital value format
@@ -18,9 +18,10 @@
  * %o -- it's a int value perse as a octal number (8 bits)
  * %x -- it's a int value perse as a hexadecimal value (16 bits) in small charactor format
  * %X -- it's a int value perse as a hexadecimal value (16 bits) in
- * %s --  it's a string display as such
+ * %s -- it's a string display as such
  * %u -- it's a unsigned integer displayes as a decimal number
  */
+/*
 $name = "Souvik Mondal";
 $string1 = "My name is Souvik Mondal" . "\n";
 $string2 = "My name is $name";
@@ -38,3 +39,22 @@ $day = Date("d");
 $month = Date("m");
 $year = Date("y");
 printf("Today is %d-%d-%d", $day, $month, $year);
+
+how many ways you can print a string in php
+1. echo constrant (this can print many values at a time)
+2. print (it can only print on value at a time)
+3. printf [this is used to generate a formatted string using the escape chractor]
+4. print_r [for debuging only]
+ */
+
+$something = <<< start
+boo
+hoo
+
+start;
+
+echo $something;
+$first_Name = "Souvik";
+$last_Name = "Mondal";
+
+echo "$first_Name", "$last_Name";
